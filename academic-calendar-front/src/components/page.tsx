56249a37795d8e71743425944e36b859ae8c3a8d
Diego Viana <dsantos@inf.ufsm.br>
2025-08-16 19:55:50 -0300

    Create page.tsx

---
+++ academic-calendar-front/src/components/page.tsx
@@ -0,0 +1,12 @@
+import { cn } from '@/libs/utils'
+import type { FormHTMLAttributes } from 'react'
+
+type PageProps = FormHTMLAttributes<HTMLDivElement>
+
+export function Page({ children, className, ...props }: PageProps) {
+  return (
+    <div className={cn('container flex-1 mx-auto my-10', className)} {...props}>
+      {children}
+    </div>
+  )
+}
