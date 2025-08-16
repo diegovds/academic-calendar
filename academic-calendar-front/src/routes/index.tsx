12f97d0f074f22491e3a249d5e2371d672bcc128
Diego Viana <dsantos@inf.ufsm.br>
2025-08-16 19:56:06 -0300

    Update index.tsx

--- academic-calendar-front/src/routes/index.tsx
+++ academic-calendar-front/src/routes/index.tsx
@@ -1,5 +1,5 @@
-import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
+import { Page } from '@/components/page'
+import { Link, createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -7,33 +7,24 @@ export const Route = createFileRoute('/')({
 
 function App() {
   return (
-    <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
-    </div>
+    <Page className="flex gap-8 items-center justify-center flex-col">
+      <h1>
+        Cupidatat elit nostrud ad consectetur. Do ex nisi ad non adipisicing
+        fugiat adipisicing elit esse tempor Lorem reprehenderit excepteur nisi.
+      </h1>
+      <p>
+        In adipisicing officia sint laboris commodo mollit ipsum consequat sunt
+        aliqua. Non aliqua id est sit sunt occaecat nisi. Et tempor mollit
+        deserunt officia. Consequat id consequat consequat duis ex fugiat
+        labore. Excepteur nostrud mollit tempor reprehenderit dolor. Id
+        consectetur velit deserunt sit. Laboris amet nostrud dolore dolor.
+        Officia dolor dolore ad fugiat et voluptate tempor cillum consequat
+        consectetur ut sunt. Cillum laborum commodo sunt consectetur est
+        excepteur fugiat. Irure magna ea velit do aliqua anim. Commodo velit
+        occaecat adipisicing non. Non et ipsum sint pariatur qui deserunt ad
+        duis nulla fugiat et irure et aliqua.
+      </p>
+      <Link to="/my-calendar">Entrar</Link>
+    </Page>
   )
 }
