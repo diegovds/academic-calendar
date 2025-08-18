6198198b92b33ecf464c54e39e18a84e9fae1297
Diego Viana <dsantos@inf.ufsm.br>
2025-08-18 19:13:07 -0300

    adjusting the layout

--- academic-calendar-front/src/routes/__root.tsx
+++ academic-calendar-front/src/routes/__root.tsx
@@ -1,6 +1,5 @@
 import type { QueryClient } from '@tanstack/react-query'
 
-import Navbar from '@/components/navbar'
 import { TanstackDevtools } from '@tanstack/react-devtools'
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
@@ -13,7 +12,6 @@ interface MyRouterContext {
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <div className="flex min-h-dvh flex-col">
-      <Navbar />
       <Outlet />
       <TanstackDevtools
         config={{
---
+++ academic-calendar-front/src/routes/_pages.tsx
@@ -0,0 +1,15 @@
+import Navbar from '@/components/navbar'
+import { Outlet, createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/_pages')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  )
+}
