beac88ec5154c1cf1aa566793b45814811dcd843
Diego Viana <dsantos@inf.ufsm.br>
2025-08-19 18:50:24 -0300

    Update page.tsx

--- academic-calendar-front/src/components/page.tsx
+++ academic-calendar-front/src/components/page.tsx
@@ -5,7 +5,10 @@ type PageProps = FormHTMLAttributes<HTMLDivElement>
 
 export function Page({ children, className, ...props }: PageProps) {
   return (
-    <div className={cn('container flex-1 mx-auto my-10', className)} {...props}>
+    <div
+      className={cn('container flex-1 mx-auto my-10 px-4 md:px-10', className)}
+      {...props}
+    >
       {children}
     </div>
   )
