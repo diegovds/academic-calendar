e5230ff18d7d8c268438f760d60eacb1e68d8bc3
Diego Viana <dsantos@inf.ufsm.br>
2025-08-19 04:43:27 -0300

    adding first version of personal page

---
+++ academic-calendar-front/src/components/my-calendar/course-create.tsx
@@ -0,0 +1,91 @@
+import { postCourses } from '@/http/api'
+import { useAuthStore } from '@/stores/useAuthStore'
+import { useModalStore } from '@/stores/useModalStore'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import toast from 'react-hot-toast'
+import z from 'zod'
+import { Button } from '../button'
+import { Form } from '../form'
+import { FormError } from '../form/form-error'
+import { FormInput } from '../form/form-input'
+import { FormItem } from '../form/form-item'
+
+const formSchema = z.object({
+  description: z.string().min(2, {
+    message: 'a descrição deve ter pelo menos 2 caracteres',
+  }),
+  title: z
+    .string()
+    .min(6, { message: 'o título deve ter pelo menos 6 caracteres' }),
+})
+
+type FormData = z.infer<typeof formSchema>
+
+type CourseCreateProps = {
+  reload: (reload: boolean) => void
+}
+
+export function CourseCreate({ reload }: CourseCreateProps) {
+  const { token } = useAuthStore()
+  const { setIsOpen } = useModalStore()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+  })
+
+  const onSubmit = async (data: FormData) => {
+    const { course } = await toast.promise(
+      postCourses(data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }),
+      {
+        loading: 'Cadastrando...',
+        success: 'Curso cadastrado!',
+        error: 'Erro ao cadastrar curso.',
+      }
+    )
+
+    if (course) {
+      setIsOpen(false)
+      reload(true)
+    }
+  }
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)} className="p-0 shadow-none">
+      <FormItem>
+        <FormInput
+          id="name"
+          type="text"
+          placeholder="Nome"
+          {...register('title')}
+        />
+        <FormError error={errors.title?.message}>
+          {errors.title?.message || '.'}
+        </FormError>
+      </FormItem>
+
+      <FormItem>
+        <FormInput
+          id="description"
+          type="text"
+          placeholder="Descrição"
+          {...register('description')}
+        />
+        <FormError error={errors.description?.message}>
+          {errors.description?.message || '.'}
+        </FormError>
+      </FormItem>
+
+      <Button type="submit" disabled={isSubmitting} className="mb-0">
+        Cadastrar
+      </Button>
+    </Form>
+  )
+}
--- academic-calendar-front/src/routes/_pages/my-calendar.tsx
+++ academic-calendar-front/src/routes/_pages/my-calendar.tsx
@@ -1,8 +1,12 @@
+import { Button } from '@/components/button'
+import { Modal } from '@/components/modal'
+import { CourseCreate } from '@/components/my-calendar/course-create'
 import { Page } from '@/components/page'
+import { type GetCourses200CoursesItem, getCourses } from '@/http/api'
 import { useAuthStore } from '@/stores/useAuthStore'
+import { useModalStore } from '@/stores/useModalStore'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { createFileRoute, useNavigate } from '@tanstack/react-router'
-import Cookies from 'js-cookie'
-import { useEffect } from 'react'
 
 export const Route = createFileRoute('/_pages/my-calendar')({
   component: MyCalendarComponent,
@@ -10,25 +14,65 @@ export const Route = createFileRoute('/_pages/my-calendar')({
 
 function MyCalendarComponent() {
   const navigate = useNavigate()
-  const { token, reset } = useAuthStore()
+  const { token, name } = useAuthStore()
+  const { setIsOpen } = useModalStore()
 
-  useEffect(() => {
-    if (token === '') {
-      navigate({ to: '/' })
+  // redireciona se não houver token
+  if (!token) {
+    navigate({ to: '/' })
+  }
+
+  const coursesQuery = useQuery<GetCourses200CoursesItem[]>({
+    queryKey: ['courses', token],
+    queryFn: async () => {
+      const res = await getCourses({
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      return res.courses
+    },
+    enabled: !!token,
+  })
+
+  const { data: courses, isLoading } = coursesQuery
+
+  const queryClient = useQueryClient()
+
+  function handleReload(reload: boolean) {
+    if (reload) {
+      queryClient.invalidateQueries({ queryKey: ['courses', token] })
     }
-  }, [token, navigate])
+  }
 
   return (
-    <Page>
-      <button
-        type="button"
-        onClick={() => {
-          reset()
-          Cookies.remove('token', { path: '/' })
-        }}
-      >
-        sair
-      </button>
+    <Page className="flex flex-col">
+      <div>
+        <h1 className="text-foreground">Olá, {name}</h1>
+      </div>
+
+      {isLoading ? (
+        <p>Carregando cursos...</p>
+      ) : courses?.length === 0 ? (
+        <div className="flex-1 flex flex-col items-center justify-center">
+          <p className="text-foreground">Você não possui cursos</p>
+          <Button
+            type="button"
+            className="w-fit px-3 p-2"
+            onClick={() => setIsOpen(true)}
+          >
+            Cadastrar curso
+          </Button>
+        </div>
+      ) : (
+        <div>
+          {courses?.map(course => (
+            <div key={course.id}>{course.title}</div>
+          ))}
+        </div>
+      )}
+
+      <Modal onClose={() => setIsOpen(false)} title="Cadastro de curso">
+        <CourseCreate reload={handleReload} />
+      </Modal>
     </Page>
   )
 }
