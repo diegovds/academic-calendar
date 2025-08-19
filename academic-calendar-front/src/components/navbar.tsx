c6e9e5e540d6987e4d3dc8a3909f0fa3c1c6eb42
Diego Viana <dsantos@inf.ufsm.br>
2025-08-19 13:30:22 -0300

    Update navbar.tsx

--- academic-calendar-front/src/components/navbar.tsx
+++ academic-calendar-front/src/components/navbar.tsx
@@ -1,17 +1,31 @@
+import { useAuthStore } from '@/stores/useAuthStore'
 import { Link } from '@tanstack/react-router'
+import Cookies from 'js-cookie'
 
 export default function Navbar() {
+  const { reset } = useAuthStore()
+
   return (
     <header className="container mx-auto p-2 flex gap-2 bg-white text-black justify-between">
       <nav className="flex flex-row">
         <div className="px-2 font-bold">
           <Link to="/">Home</Link>
         </div>
-        <div className="px-2 font-bold">
-          <Link to="/sign-up">Cadastro</Link>
-        </div>
+
         <div className="px-2 font-bold">
           <Link to="/my-calendar">Meu calendario</Link>
+        </div>
+
+        <div className="px-2 font-bold">
+          <button
+            type="button"
+            onClick={() => {
+              reset()
+              Cookies.remove('token', { path: '/' })
+            }}
+          >
+            Sair
+          </button>
         </div>
       </nav>
     </header>
