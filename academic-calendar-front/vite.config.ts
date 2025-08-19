e039605bb330ea6606544186df312e54b0e54cac
Diego Viana <dsantos@inf.ufsm.br>
2025-08-19 13:08:25 -0300

    Update vite.config.ts

--- academic-calendar-front/vite.config.ts
+++ academic-calendar-front/vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import viteReact from '@vitejs/plugin-react'
 import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,5 +19,9 @@ export default defineConfig({
     alias: {
       '@': resolve(__dirname, './src'),
     },
+  },
+  server: {
+    host: true,
+    open: true,
   },
 })
