2c97d20cb0fcf2300a876e81ece4a766cff50dbc
Diego Viana <dsantos@inf.ufsm.br>
2025-08-20 17:52:42 -0300

    Update useTaskStore.ts

--- academic-calendar-front/src/stores/useTaskStore.ts
+++ academic-calendar-front/src/stores/useTaskStore.ts
@@ -2,12 +2,16 @@ import { create } from 'zustand'
 
 interface TaskState {
   task: string
+  name: string
   setTask: (task: string) => void
-  toggleTask: () => void
+  setName: (task: string) => void
+  reset: () => void
 }
 
 export const useTaskStore = create<TaskState>(set => ({
   task: '',
+  name: '',
   setTask: value => set({ task: value }),
-  toggleTask: () => set({ task: '' }),
+  setName: value => set({ name: value }),
+  reset: () => set({ task: '', name: '' }),
 }))
