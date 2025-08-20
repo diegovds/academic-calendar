a68858c61cfaee459ba451960159059d0242fc82
Diego Viana <dsantos@inf.ufsm.br>
2025-08-20 05:26:21 -0300

    Update useModalStore.ts

--- academic-calendar-front/src/stores/useModalStore.ts
+++ academic-calendar-front/src/stores/useModalStore.ts
@@ -2,12 +2,18 @@ import { create } from 'zustand'
 
 interface AuthState {
   isOpen: boolean
+  whoOpened: string
   setIsOpen: (isOpen: boolean) => void
+  setWhoOpened: (whoOpened: string) => void
   toggleIsOpen: () => void
+  toggleWhoOpened: () => void
 }
 
 export const useModalStore = create<AuthState>(set => ({
   isOpen: false,
+  whoOpened: '',
   setIsOpen: value => set({ isOpen: value }),
+  setWhoOpened: value => set({ whoOpened: value }),
   toggleIsOpen: () => set(state => ({ isOpen: !state.isOpen })),
+  toggleWhoOpened: () => set({ whoOpened: '' }),
 }))
