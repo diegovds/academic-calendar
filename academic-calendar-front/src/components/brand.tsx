c583ad0d92df77e82d3bcda8a750c84dcf4d9ae7
Diego Viana <dsantos@inf.ufsm.br>
2025-08-20 23:49:16 -0300

    Create brand.tsx

---
+++ academic-calendar-front/src/components/brand.tsx
@@ -0,0 +1,5 @@
+export function Brand() {
+  return (
+    <div className="w-1 rounded-full transition-colors duration-300 bg-gradient-to-b from-blue-500 to-green-600 group-hover:from-green-500 group-hover:to-blue-600" />
+  )
+}
