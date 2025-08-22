0cc52e62b064d5858a27a03564b7109eca53c904
Diego Viana <dsantos@inf.ufsm.br>
2025-08-21 21:59:28 -0300

    fix prod front

--- academic-calendar-front/vite.config.ts
+++ academic-calendar-front/vite.config.ts
@@ -1,27 +1,22 @@
 import tailwindcss from '@tailwindcss/vite'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tanstackRouter from '@tanstack/router-plugin/vite'
 import viteReact from '@vitejs/plugin-react'
-import { resolve } from 'node:path'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
+    tanstackRouter({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
   ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
     },
   },
-  server: {
-    host: true,
-    open: true,
-  },
+  server: { host: true, open: true },
 })
