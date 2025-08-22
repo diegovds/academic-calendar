74e3aad69df79ea44b95218fdfe48271b5fb2269
Diego Viana <dsantos@inf.ufsm.br>
2025-08-21 22:15:28 -0300

    Update __root.tsx

--- academic-calendar-front/src/routes/__root.tsx
+++ academic-calendar-front/src/routes/__root.tsx
@@ -24,18 +24,20 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         }}
         richColors
       />
-      <TanstackDevtools
-        config={{
-          position: 'bottom-left',
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-          TanStackQueryDevtools,
-        ]}
-      />
+      {import.meta.env.DEV && (
+        <TanstackDevtools
+          config={{
+            position: 'bottom-left',
+          }}
+          plugins={[
+            {
+              name: 'Tanstack Router',
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+            TanStackQueryDevtools,
+          ]}
+        />
+      )}
     </div>
   ),
 })
