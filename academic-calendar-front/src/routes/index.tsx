daa4fad08eaa3d3bcbb76d15518c66fcd173aa9b
Diego Viana <dsantos@inf.ufsm.br>
2025-08-22 21:08:14 -0300

    Update index.tsx

--- academic-calendar-front/src/routes/index.tsx
+++ academic-calendar-front/src/routes/index.tsx
@@ -54,8 +54,8 @@ function Homepage() {
                 Cadastre seus cursos, semestres, disciplinas e não perca nenhum
                 trabalho ou prova.
               </p>
-              <div className="bg-blue-200 w-fit place-self-center p-2.5 rounded-full">
-                <LiaBookSolid className="text-blue-600 text-2xl lg:text-3xl" />
+              <div className="bg-blue-100 w-fit place-self-center p-2.5 rounded-full">
+                <LiaBookSolid className="text-blue-500 text-2xl lg:text-3xl" />
               </div>
             </div>
             <div className="flex flex-1 flex-col-reverse p-4 gap-4 rounded bg-background shadow shadow-gray-300">
@@ -63,8 +63,8 @@ function Homepage() {
                 Você tem tudo em um só lugar para estudar com mais foco e menos
                 estresse.
               </p>
-              <div className="bg-blue-200 w-fit place-self-center p-2.5 rounded-full">
-                <PiStudentFill className="text-blue-600 text-2xl lg:text-3xl" />
+              <div className="bg-blue-100 w-fit place-self-center p-2.5 rounded-full">
+                <PiStudentFill className="text-blue-500 text-2xl lg:text-3xl" />
               </div>
             </div>
           </div>
