ba001790b37797f99781971568959d9db11eecd7
Diego Viana <dsantos@inf.ufsm.br>
2025-08-26 18:01:22 -0300

    Update discipline-container.tsx

--- academic-calendar-front/src/routes/_pages/course/-components/discipline-container.tsx
+++ academic-calendar-front/src/routes/_pages/course/-components/discipline-container.tsx
@@ -116,7 +116,6 @@ export function DisciplineContainer({ semesterId }: DisciplineContainerProps) {
         <Modal
           onClose={() => {
             setIsOpen(false)
-            setSelectedDiscipline(null)
           }}
           title={
             selectedDiscipline
