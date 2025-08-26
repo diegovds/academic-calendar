64705eb914ef0b46cbd105df9c6a91e26a1d361c
Diego Viana <dsantos@inf.ufsm.br>
2025-08-26 15:18:00 -0300

    Update _pages.tsx

--- academic-calendar-front/src/routes/_pages.tsx
+++ academic-calendar-front/src/routes/_pages.tsx
@@ -1,4 +1,3 @@
-import { Footer } from '@/components/footer'
 import Navbar from '@/components/navbar'
 import { Outlet, createFileRoute } from '@tanstack/react-router'
 
@@ -11,7 +10,6 @@ function RouteComponent() {
     <>
       <Navbar />
       <Outlet />
-      <Footer />
     </>
   )
 }
