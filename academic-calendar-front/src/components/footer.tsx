da25eb966c66982a5c99c4827e5067ca3dd056d3
Diego Viana <dsantos@inf.ufsm.br>
2025-08-26 16:59:44 -0300

    Update footer.tsx

--- academic-calendar-front/src/components/footer.tsx
+++ academic-calendar-front/src/components/footer.tsx
@@ -1,14 +1,21 @@
 import { Heart } from 'lucide-react'
+import { ModeToggle } from './ui/mode-toggle'
 
 export function Footer() {
   return (
-    <footer className="bg-background shadow p-2">
-      <span className="mx-auto flex w-fit items-center gap-1.5 text-xs text-foreground tracking-wider">
-        Feito com
-        <Heart size={13} className="fill-foreground text-foreground" />
-        por
-        <strong className="font-medium">Diego Viana</strong>
-      </span>
+    <footer className="bg-secondary shadow shadow-gray-300">
+      <div className="relative mx-auto flex items-center container px-4 md:px-10 py-2">
+        <span className="absolute left-1/2 -translate-x-1/2 flex w-fit items-center gap-1.5 text-xs text-foreground tracking-wider">
+          Feito com
+          <Heart size={13} className="fill-foreground text-foreground" />
+          por
+          <strong className="font-medium">Diego Viana</strong>
+        </span>
+
+        <div className="ml-auto">
+          <ModeToggle />
+        </div>
+      </div>
     </footer>
   )
 }
