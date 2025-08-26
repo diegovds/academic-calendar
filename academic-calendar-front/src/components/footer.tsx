3c9d8eb6784355008fb5acee8f8a9500794888b6
Diego Viana <dsantos@inf.ufsm.br>
2025-08-26 18:15:41 -0300

    Update footer.tsx

--- academic-calendar-front/src/components/footer.tsx
+++ academic-calendar-front/src/components/footer.tsx
@@ -3,16 +3,16 @@ import { ModeToggle } from './ui/mode-toggle'
 
 export function Footer() {
   return (
-    <footer className="bg-secondary shadow shadow-gray-300">
+    <footer className="bg-secondary shadow shadow-gray-300 rotate-180">
       <div className="relative mx-auto flex items-center container px-4 md:px-10 py-2">
-        <span className="absolute left-1/2 -translate-x-1/2 flex w-fit items-center gap-1.5 text-xs text-foreground tracking-wider">
+        <span className="absolute rotate-180 left-1/2 -translate-x-1/2 flex w-fit items-center gap-1.5 text-xs text-foreground tracking-wider">
           Feito com
           <Heart size={13} className="fill-foreground text-foreground" />
           por
           <strong className="font-medium">Diego Viana</strong>
         </span>
 
-        <div className="ml-auto">
+        <div className="mr-auto rotate-180">
           <ModeToggle />
         </div>
       </div>
